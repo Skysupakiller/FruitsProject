510d6daa0f023c65f98f3ceca107225dc7e1cdbf
chris <chris.turp1@gmail.com>
2021-10-29 11:52:04 +0200

    Init MongoDB

---
+++ app.js
@@ -0,0 +1,48 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'fruitsDB';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  insertDocuments(db, function() {
+    client.close();
+  });
+});
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Insert some documents
+  collection.insertMany([{
+    name: "Apple",
+    score: 5,
+    review: "hum ok"
+  }, {
+    name: "Orange",
+    score: 6,
+    review: "Good when sweet"
+  }, {
+    name: "Banana",
+    score: 8,
+    review: "loven Them"
+  }], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.insertedCount);
+    assert.equal(3, Object.keys(result.insertedIds).length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
